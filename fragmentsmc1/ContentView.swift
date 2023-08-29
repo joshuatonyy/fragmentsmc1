09642b6b8e289407d3365b70f67833c49712dcb7
Joshua Tony <64730665+joshuatonyy@users.noreply.github.com>
2023-08-29 15:17:22 +0700

    Initial Commit

---
+++ fragmentsmc1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  fragmentsmc1
+//
+//  Created by Joshua on 29/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
