69e863266bd9c3719c09d6423be91070669872f7
Patricia Ho <hopatricia1711@gmail.com>
2023-09-03 14:35:37 +0700

    Add History View and Component, Add Result View and Component, Add Asset for Sidebar, Make Sidebar, and Add Accent Color

---
+++ fragmentsmc1/Views/History/View/HistoryView.swift
@@ -0,0 +1,53 @@
+//
+//  HistoryView.swift
+//  fragmentsmc1
+//
+//  Created by Patricia Ho on 29/08/23.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    @State private var unhealthFoods = Journal.unhealthyExamples()
+    
+    var body: some View {
+        VStack {
+            List {
+               ForEach(unhealthFoods) { journal1 in
+                   JournalRow(journal: journal1)
+               }
+              
+               .listRowSeparator(.hidden)
+               .listRowInsets(.init(top: 0,         leading: 0,    bottom: 2,      trailing: 0))
+               
+               .listRowBackground(
+                   RoundedRectangle(cornerRadius: 10)
+                    .background(Color.Neutral.s20)
+                    .foregroundColor(Color.Neutral.s10)
+                       .padding(
+                           EdgeInsets(
+                               top: 5,
+                               leading: 10,
+                               bottom: 5,
+                               trailing: 10
+                           )
+                       )
+               )
+               
+           }
+           .listSectionSeparator(.hidden)
+           .environment(\.defaultMinListRowHeight, 150)
+           .listStyle(.plain)
+
+        }
+        .padding([.leading, .trailing], 20)
+        .background(Color.Neutral.s20)
+        
+    }
+}
+
+struct HistoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        HistoryView()
+    }
+}
